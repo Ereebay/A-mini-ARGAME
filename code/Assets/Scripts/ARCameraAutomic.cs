2c8b2c843fc821a6ce95f8efa1df552324c9017d
Ereebay <xk.jackson@hotmail.com>
2016-09-10 21:36:43 +0800

    修正了自动聚焦
    修正了自动聚焦

---
+++ code/Assets/Scripts/ARCameraAutomic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ARCameraAutomic : MonoBehaviour
+{
+    // Use this for initialization  
+    void Start()
+    {
+        Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)  
+        {
+            Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
